9d9c2dab983939168cc7cfcb13a07b58a3e5e22c
Shalin Ahasan <shalin.a.ahasan@gmail.com>
2025-01-26 21:15:57 +0000

    Deploy files

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, jsonify, send_file, abortimport os
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
